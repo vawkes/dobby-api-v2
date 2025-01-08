0ab4a16385d0e04206aab5e2eb26a33cd16c10d8
biskeyevan <75827323+biskeyevan@users.noreply.github.com>
2025-01-07 20:17:34 -0800

    Initial commit

---
+++ test/dobby-api-v2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DobbyApiV2 from '../lib/dobby-api-v2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dobby-api-v2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DobbyApiV2.DobbyApiV2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
