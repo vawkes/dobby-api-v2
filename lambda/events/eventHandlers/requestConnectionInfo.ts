c8da35b6aabea9821f7a320fe661d1620c6f0db7
biskeyevan <75827323+biskeyevan@users.noreply.github.com>
2025-06-16 08:15:16 -0700

    fix

--- lambda/events/eventHandlers/requestConnectionInfo.ts
+++ lambda/events/eventHandlers/requestConnectionInfo.ts
@@ -18,30 +18,8 @@ export const handleRequestConnectionInfo = async (eventData: RequestConnectionIn
     // Command Type
     view.setUint8(0, EventMap.REQUEST_CONNECTION_INFO);
 
-    // Send the command to the device and get response
-    const response = await sendToDobby(eventData.device_id, buffer);
-
-    // Parse the response data
-    let lastRxRssi: number | undefined;
-    let lastRxSnr: number | undefined;
-    let lastRxLinkType: number | undefined;
-
-    if (response && response.length >= 7) {  // We expect at least 7 bytes in response
-        const responseView = new DataView(response);
-
-        // Check response type (should be 7)
-        const responseType = responseView.getUint8(0);
-        if (responseType === 7) {
-            // Parse RSSI (signed int8)
-            lastRxRssi = responseView.getInt8(1);
-
-            // Parse SNR (signed int8)
-            lastRxSnr = responseView.getInt8(2);
-
-            // Parse Link Type (4 bytes)
-            lastRxLinkType = responseView.getUint32(3, true);  // true for little-endian
-        }
-    }
+    // Send the command to the device
+    await sendToDobby(eventData.device_id, buffer);
 
     // Create the event object
     const event: EventSchemaType = {
@@ -49,10 +27,7 @@ export const handleRequestConnectionInfo = async (eventData: RequestConnectionIn
         event_type: EventType.REQUEST_CONNECTION_INFO,
         event_data: {
             device_id: eventData.device_id,
-            event_sent: true,
-            last_rx_rssi: lastRxRssi,
-            last_rx_snr: lastRxSnr,
-            last_rx_link_type: lastRxLinkType
+            event_sent: true
         },
         event_ack: false
     };
